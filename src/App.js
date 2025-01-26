3d0af96f1fe66a5c51031352838aab13bfbe9ffd
pushpak chavan <pushpak1991chavan@gmail.com>
2025-01-26 22:52:58 +0530

    initial header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-     <h1>Images Gallery</h1>
+    <Header title="Images Gallery"/>
     </div>
   );
 }
