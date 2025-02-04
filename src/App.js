8d49b0b5d85bd84db5c56b86839a161aba4b24b9
pushpak chavan <pushpak1991chavan@gmail.com>
2025-02-04 20:01:45 +0530

    eslint and prettier setup

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
-
-const UNSPLASH_KEY=process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
-  
- const [ word, setWord] = useState('');
- const handleSearchSubmit=(e)=>{
-  e.preventDefault();
-  console.log(word);
-  fetch(`https://api.unsplash.com//photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-  .then((res)=> res.json())
-  .then((data)=>{
-    console.log(data)
-  })
+  const [word, setWord] = useState("");
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+    fetch(
+      `https://api.unsplash.com//photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    );
+     .then((res) => res.json());
+      .then((data) => {
+        console.log(data);
+      });
 
-
-  .catch((err)=>{
-    console.log(err);
-  });
-
-}
-
+      .catch((err) => {
+        console.log(err);
+      });
+    setWord(""); 
+  };
 
   return (
     <div className="App">
-    <Header title="Images Gallery"/> 
-    <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
