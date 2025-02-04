7d89052209f48716cd989b90c17ce83ecd4a9f3b
pushpak chavan <pushpak1991chavan@gmail.com>
2025-02-04 21:01:31 +0530

    adjust vs code tab size

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,17 @@
-import  React from 'react';
+import React from 'react';
 
-import {Navbar, Container} from 'react-bootstrap';
-const navbarstyle={
-backgroundColor :'#007bff'
+import { Navbar, Container } from 'react-bootstrap';
+const navbarstyle = {
+  backgroundColor: '#007bff',
 };
 
-const Header =({title})=>{
-   
-    return(
-        <Navbar style={navbarstyle} variant="light">
-            <Container>
-               <Navbar.Brand href="/">{title}</Navbar.Brand>
-            </Container>
-            
-
-
-        </Navbar>
-    )
-
+const Header = ({ title }) => {
+  return (
+    <Navbar style={navbarstyle} variant="light">
+      <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 export default Header;
